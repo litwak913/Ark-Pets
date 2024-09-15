6738deb4c19dc775c5970134f49d8dd83fbbfc11
Harry Huang <harryhuang2652@qq.com>
2024-09-15 23:39:52 +0800

    refactor: move platform package

--- core/src/cn/harryh/arkpets/platform/User32HWndCtrl.java
+++ core/src/cn/harryh/arkpets/platform/User32HWndCtrl.java
@@ -1,7 +1,7 @@
 /** Copyright (c) 2022-2024, Harry Huang, Litwak913
  * At GPL-3.0 License
  */
-package cn.harryh.arkpets.hwnd;
+package cn.harryh.arkpets.platform;
 
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
